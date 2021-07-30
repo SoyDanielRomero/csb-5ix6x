9abb15aee497c14f7f7c46af3d5dfbc1549cacc9
Code Sandbox <hello@codesandbox.io>
2021-07-30 09:46:20 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a57557f4dae36f197668e397c924c871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e22f4d8c75ff31ab9a3",
+    "url": "https://soydanielromero.github.io/csb-5ix6x/static/css/main.25328605.chunk.css"
+  },
+  {
+    "revision": "2e22f4d8c75ff31ab9a3",
+    "url": "https://soydanielromero.github.io/csb-5ix6x/static/js/main.2e22f4d8.chunk.js"
+  },
+  {
+    "revision": "2b58efce8fe4aa6be07f",
+    "url": "https://soydanielromero.github.io/csb-5ix6x/static/js/1.2b58efce.chunk.js"
+  },
+  {
+    "revision": "ce78c2980f20d3138cd8",
+    "url": "https://soydanielromero.github.io/csb-5ix6x/static/js/runtime~main.ce78c298.js"
+  },
+  {
+    "revision": "4bf04142879148fe1f300bccf3cedb1a",
+    "url": "https://soydanielromero.github.io/csb-5ix6x/index.html"
+  }
+];
